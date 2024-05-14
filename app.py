8b554b89502e4267b8385bcc65b82e69b3f4b27e
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-13 23:19:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 import streamlit as st
 
-def simp_add(n1,n2):
-  n3 = n1 + n2
-  return n3
+def simp_add(n1, n2):
+    n1 = float(n1)
+    n2 = float(n2)
+    n3 = n1 + n2
+    return n3
 
-def simp_sub( n1,n2):
-  n3 = n1 - n2
-  return n3
+def simp_sub(n1, n2):
+    n1 = float(n1)
+    n2 = float(n2)
+    n3 = n1 - n2
+    return n3
 
-n1 = st.text_area("what is your first number ?")
-n2 = st.text_area("what is your second number ?")
-n4 = st.text_area("what is you want numbers to do, sum (write add) or subtract (write sub)")
+n1 = st.text_area("What is your first number?")
+n2 = st.text_area("What is your second number?")
+n4 = st.text_area("What operation do you want to perform? Type 'add' for addition or 'sub' for subtraction.")
 
 if n4 == "add":
-    st.markdown(simp_add(n1,n2))
+    st.markdown("Result: " + str(simp_add(n1, n2)))
 elif n4 == "sub":
-    st.markdown(simp_sub(n1,n2))
-else
+    st.markdown("Result: " + str(simp_sub(n1, n2)))
+else:
     st.markdown("Error in selection.")
