fcda40e02a3236e744b841525e1ef97c8e309a54
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-13 23:37:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ n1 = st.text_area("What is your first number?")
 n2 = st.text_area("What is your second number?")
 n4 = st.text_area("What operation do you want to perform? Type 'add' for addition or 'sub' for subtraction.")
 
-if n4 == "add":
+if st.button (n4 == "add"):
     st.markdown("Result: " + str(simp_add(n1, n2)))
-elif n4 == "sub":
+elif st.button(n4 == "sub"):
     st.markdown("Result: " + str(simp_sub(n1, n2)))
 else:
     st.markdown("Error in selection.")
